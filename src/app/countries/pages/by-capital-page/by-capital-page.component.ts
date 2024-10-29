17bc5504d23d421f1ac2b0a0758b2fbdce4f3ee8
RichardRodri26 <richardmanuel811@gmail.com>
2024-10-28 20:50:23 -0500

    adding countries services

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = []
+
+  constructor(private countriesService: CountriesService) {}
 
   searchByCapital(term: string): void {
-    console.log('term', term)
+    this.countriesService.searchCapital(term).subscribe(countries => {
+      this.countries = countries
+    });
   }
-
 }
